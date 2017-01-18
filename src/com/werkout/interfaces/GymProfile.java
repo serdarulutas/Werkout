6cb41d3bcd1807cd86bf2d9513764f4300c8cb4c
serdarulutas <serdar.ulutas@gmail.com>
2017-01-18 14:28:59 -0500

    another update

--- src/com/werkout/interfaces/GymProfile.java
+++ src/com/werkout/interfaces/GymProfile.java
@@ -3,11 +3,18 @@ package com.werkout.interfaces;
 
 
 public interface GymProfile{
-	public boolean isEmpty();
+	//Iterators
+	public boolean isEmpty();	
 	public Equipment first();
 	public Equipment next();
+	public boolean isLast(); //returns if the current item is the last one.
 	public Equipment currentItem();
-	public boolean isLast(); //returns if the current item is the last one. 
 	
+	//Modifications. Order is not important
+	public void addEquipment(Equipment equipment);
+	public void removeEquipment(Equipment equipment);
+	public void setName(String name);	
 	
+	//return xml for the configuration file. 
+	public String toXML();
 }
