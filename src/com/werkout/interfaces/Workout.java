9659de5e8f118ac0e9be1b71838a05a915f09e9b
serdarulutas <serdar.ulutas@gmail.com>
2017-01-18 11:44:57 -0500

    current changes (still testing git)

--- src/com/werkout/interfaces/Workout.java
+++ src/com/werkout/interfaces/Workout.java
@@ -1,9 +1,11 @@
 package com.werkout.interfaces;
 
+
+
 public interface Workout {
 	
 	public int getID();
 	public Media getMedia(); 
 	public int getDifficulty(); //in percentage (1..100)
-	public String getWorkoutType(); //CHEST,LEGS, CARDIO 
+	public WorkoutArea getWorkoutType(); //CHEST,LEGS, CARDIO 
 }
---
+++ src/com/werkout/interfaces/WorkoutArea.java
@@ -0,0 +1,5 @@
+package com.werkout.interfaces;
+
+public enum WorkoutArea {
+	LEGS,CHEST,ABS,ARMS,CARDIO,WHOLEBODY,BACK
+}
