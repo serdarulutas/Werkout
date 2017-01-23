06279c046f0a26b4d6132b0372cf50321c4ba3d6
Serdar <Serdar@SerdarPC>
2017-01-22 22:35:26 -0500

    changes sunday

---
+++ src/com/werkout/interfaces/WorkoutPlaylistMedia.java
@@ -0,0 +1,8 @@
+package com.werkout.interfaces;
+
+public interface WorkoutPlaylistMedia {
+	public Media getMedia();
+	public int getDuration();
+	public boolean isRandom();
+	public boolean isRest();
+}
