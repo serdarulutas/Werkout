bc3e3098a10e6e20de44686d6aafa155fa7b03c4
Serdar <Serdar@SerdarPC>
2017-08-25 21:26:08 -0400

    11

--- src/com/werkout/model/WorkoutArea.java
+++ src/com/werkout/model/WorkoutArea.java
@@ -11,4 +11,29 @@ public class WorkoutArea {
 	private String name;
 	private HashMap<String, WorkoutEquipment> equipment;
 	private Equipment_Status equipStatus;
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Equipment_Status getEquipStatus() {
+		return equipStatus;
+	}
+	public void setEquipStatus(Equipment_Status equipStatus) {
+		this.equipStatus = equipStatus;
+	}
+	public void addEquipment(WorkoutEquipment equip){
+		if (equipment == null)
+			equipment = new HashMap<String, WorkoutEquipment>();
+		equipment.put(equip.getId(), equip);
+	}	
 }
